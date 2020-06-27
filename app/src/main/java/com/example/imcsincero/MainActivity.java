bd69270363c2891e9898d01b43c98a7d0bc0a1f5
lecs01 <lecs.01@outlook.com>
2020-06-26 22:09:37 -0300

    implementacao do codigo do MainActivity

--- app/src/main/java/com/example/imcsincero/MainActivity.java
+++ app/src/main/java/com/example/imcsincero/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.imcsincero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editTextAltura = findViewById(R.id.editTextAltura);
+        final EditText editTextPeso = findViewById(R.id.editTextPeso);
+
+        Button calcular = findViewById(R.id.btnCalcular);
+        calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double altura = Double.parseDouble(Objects.requireNonNull(editTextAltura.getText().toString()));
+                double peso = Double.parseDouble(Objects.requireNonNull(editTextPeso.getText().toString()));
+
+                double imc = peso / (altura * altura);
+
+                Intent intent = new Intent(getApplicationContext(),IMCResultado.class);
+                intent.putExtra("IMC", imc);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
+
 }
