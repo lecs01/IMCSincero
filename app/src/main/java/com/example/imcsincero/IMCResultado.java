1dd23909712275c3966c07c9470f230a4f74f488
lecs01 <lecs.01@outlook.com>
2020-06-29 22:02:35 -0300

    correção da classe IMCResultado

--- app/src/main/java/com/example/imcsincero/IMCResultado.java
+++ app/src/main/java/com/example/imcsincero/IMCResultado.java
@@ -3,6 +3,7 @@ package com.example.imcsincero;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,29 +19,33 @@ public class IMCResultado extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.imc_resultado);
 
+        final ImageView imgResultado = findViewById(R.id.imgResultado);
+        final TextView textIMC = findViewById(R.id.textIMC);
+        final TextView textSincero = findViewById(R.id.textSincero);
+
         double imc = getIntent().getDoubleExtra("IMC", 0D);
 
         if (imc < 18.5) {
-            imgResultado.setImageResource(R.drawable.imc5);
-            textIMC.setText("Chassi do Grilo ou Filé da Borboleta!");
-            textSincero.setText("Cuidado! Nesse Ritmo quando você estiver de frente, vai parecer que está de lado e quando estiver de lado, vai parecer que sumiu!.<br> Aproveite a oportunidade e faça um tour pela praça de Alimentação do Shopping!");
+            imgResultado.setImageResource(R.drawable.imc1);
+            textIMC.setText("Filé de Borboleta!");
+            textSincero.setText("Cuidado! Nesse Ritmo quando você estiver de frente, vai parecer que está de lado e quando estiver de lado, vai parecer que sumiu!. Aproveite a oportunidade e faça um tour pela praça de Alimentação do Shopping!");
         } else if (imc < 25){
-            imgResultado.setImageResource(R.drawable.imc3);
+            imgResultado.setImageResource(R.drawable.imc2);
             textIMC.setText("Que Físico!");
-            textSincero.setText("Como diz o ditado: 'em time que está ganhando, não se mexe!<br> Continue assim e terá idade pra ver os bisnetos!");
+            textSincero.setText("Como diz o ditado: 'em time que está ganhando, não se mexe! Continue assim e terá idade pra ver os bisnetos!");
                     } else if (imc < 30){
-            imgResultado.setImageResource(R.drawable.imc7);
+            imgResultado.setImageResource(R.drawable.imc3);
             textIMC.setText("Ops!");
-            textSincero.setText("Escorregou numa lasanha por esses dias né?! <br> Não deixe que ela te vença. Lute!");
+            textSincero.setText("Escorregou numa lasanha por esses dias né?! Não deixe que ela te vença. Você que Lute!");
         } else {
-            imgResultado.setImageResource(R.drawable.imc8);
+            imgResultado.setImageResource(R.drawable.imc4);
             textIMC.setText("Deu m&rd@");
-            textSincero.setText("Lascou!<br> Invoque o Edward Mãos de Tesoura que há em você e corte tudo menos água e... feche a boquinha, please!");
+            textSincero.setText("Lascou! Invoque o Edward Mãos de Tesoura que há em você e corte tudo menos água e... feche a boquinha, please!");
         }
     }
 
     public void voltar(View view){
-        Button voltar = findViewById(R.id.btnVoltar);
+        Button botaovoltar = findViewById(R.id.btnVoltar);
         finish();
     }
 }
